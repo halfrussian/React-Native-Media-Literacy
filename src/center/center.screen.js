8f30ac5502a0cb525d4c2abff6d4985bbb49308a
Joshua Bracken <joshua.bracken@kingmetals.com>
2021-12-03 16:05:09 -0600

    first commit

---
+++ src/center/center.screen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+
+export const CenterScreen = () => {
+
+    return (
+        <>
+        <View>
+            <Text>Center Screen</Text>
+        </View>
+        </>
+    )
+}
